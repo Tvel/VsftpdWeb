316cca8ee9f8e22bf6a8674ff3094b9fbe5e630b
Tosil Velkoff <tosilv@gmail.com>
2014-01-14 18:40:31 +0200

    Log page now displays errors for missing or empty log file.
    Log page now displays errors for missing or empty log file. Useful for
    installation time.

--- application/views/log/index.php
+++ application/views/log/index.php
@@ -6,68 +6,71 @@
 <?php
 		// $myfile should be log file path
 	$myfile = $log_path;
-	if(file_exists($myfile)){
+	if (file_exists($myfile) ) { 
+		if (filesize($myfile) > 0 ) {
 
-		exec("tac $myfile /var/www/ftp/temp.txt");
-		$ic = 0;
-		$ic_max = 200;  // stops after this number of rows
-		$handle = popen( "tac $myfile " , "r");
-		while (!feof($handle) && ++$ic<=$ic_max) {
-		//for ($ic = -1; $ic >-101; $ic-- ) {
-		$buffer = fgets($handle, 4096);
-		
-		
-		
-		
-	//size
-		$size = strstr($buffer, "/", true);
-		$pos = strrpos($size, ".")+ 1;
-		
-		$size = substr($size,$pos);
-		$size = strstr($size, " ");
-		
-	$si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
-	$base = 1024;
-	$class = min((int)log((int)$size , $base) , count($si_prefix) - 1);
-	$msize = sprintf('%1.2f' , $size / pow($base,$class)) . ' ' . $si_prefix[$class];
-	//
-		
-	//name
-	$name = strstr($buffer, "/");
-	$name = strstr($name, " _ ", true);
-	$name = substr($name,0,-1);
-	//
+			exec("tac $myfile /var/www/ftp/temp.txt");
+			$ic = 0;
+			$ic_max = 200;  // stops after this number of rows
+			$handle = popen( "tac $myfile " , "r");
+			while (!feof($handle) && ++$ic<=$ic_max) {
+			//for ($ic = -1; $ic >-101; $ic-- ) {
+			$buffer = fgets($handle, 4096);
+			
+			
+			
+			
+			//size
+			$size = strstr($buffer, "/", true);
+			$pos = strrpos($size, ".")+ 1;
+			
+			$size = substr($size,$pos);
+			$size = strstr($size, " ");
+			
+			$si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
+			$base = 1024;
+			$class = min((int)log((int)$size , $base) , count($si_prefix) - 1);
+			$msize = sprintf('%1.2f' , $size / pow($base,$class)) . ' ' . $si_prefix[$class];
+			//
+			
+			//name
+			$name = strstr($buffer, "/");
+			$name = strstr($name, " _ ", true);
+			$name = substr($name,0,-1);
+			//
 
-	//state and user
-		$state = strstr($buffer, " _ ");
-		$user = $state;
-		$state = strstr($state, "g", true);
-		$state = substr($state,3, -1);
-		if ($state == 'i') $state = 'Uploaded';
-		else if ($state == 'o') $state = 'Downloaded';
+			//state and user
+			$state = strstr($buffer, " _ ");
+			$user = $state;
+			$state = strstr($state, "g", true);
+			$state = substr($state,3, -1);
+			if ($state == 'i') $state = 'Uploaded';
+			else if ($state == 'o') $state = 'Downloaded';
+			
+			$user = strstr($user, "g");
+			$user = strstr($user, "ftp", true);
+			$user = substr($user,2, -1);
+			//b _ i
 		
-		$user = strstr($user, "g");
-		$user = strstr($user, "ftp", true);
-		$user = substr($user,2, -1);
-	//b _ i
-	
-		$info = strstr($buffer, $size, true);
-		
-		
-		echo "<tr>
-		
-		<td>".$info."</td>
-		<td>".$msize."</td>
-		<td>".$state."</td>
-		<td>".$user."</td>
-		<td>".$name."</td>
-		</tr>";
-		
-		
-		
-		}
-		pclose($handle);
-	} else { echo "<h2>---Log file path is incorrect!!---</h2>";}
+			$info = strstr($buffer, $size, true);
+			
+			
+			echo "<tr>
+			
+			<td>".$info."</td>
+			<td>".$msize."</td>
+			<td>".$state."</td>
+			<td>".$user."</td>
+			<td>".$name."</td>
+			</tr>";
+			
+			
+			
+			}
+			pclose($handle);
+
+		} else { echo "<h2>---Log file is empty---</h2>";}
+	} else { echo "<h2>---Log file path is incorrect---</h2>";}
 
 ?>
 </table>
